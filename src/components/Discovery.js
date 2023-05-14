488a692b518f85df6388b60f679f96a69a65a16a
nur <mirov0727@gmail.com>
2023-05-14 22:44:56 +0800

    Adding Logo and Body component

--- src/components/Discovery.js
+++ src/components/Discovery.js
@@ -1,16 +1,17 @@
 import Excel from "./Excel";
+import Logo from "./Logo";
+import Body from "./Body";
 
 function Discovery() {
     return (
-        <div>
-            <h2>Excel</h2>
-            <Excel
-                headers={['Name', 'Year']}
-                initialData={[
-                    ['Charles', '1859'],
-                    ['Antonie', '1943'],
-                ]}
-            />
+        <div className="Discovery">
+            <h2>Logo</h2>
+            <div style={{ background: '#f6f6f6', display: 'inline-block' }}>
+                <Logo />
+            </div>
+
+            <h2>Body</h2>
+            <Body>I am content inside the body</Body>
         </div>
     )
 }
