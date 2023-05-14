feb588ef99264507b43e6a8adc4bcdd72d4e3d1e
nur <mirov0727@gmail.com>
2023-05-14 23:35:30 +0800

    Add the suggets component

--- src/components/Discovery.js
+++ src/components/Discovery.js
@@ -2,6 +2,7 @@ import Excel from "./Excel";
 import Logo from "./Logo";
 import Body from "./Body";
 import Button from './Button';
+import Suggest from './Suggest';
 import './Discovery.css';
 
 function Discovery() {
@@ -26,6 +27,11 @@ function Discovery() {
             <p>
                 Custom class name:{''}
                 <Button className="Discovery-custom-button">I do nothing</Button>
+            </p>
+
+            <h2>Suggest</h2>
+            <p>
+                <Suggest options={['eenie', 'meenie', 'miney', 'mo']} />
             </p>
         </div>
     )
